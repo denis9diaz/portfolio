4886494457005ad1bab156245ef252bbb2eb4649
Denis <denis9diaz@hotmail.com>
2025-01-23 17:07:28 +0100

    terminado technologies

--- src/front/js/Technologies.js
+++ src/front/js/Technologies.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import '../styles/technologies.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import bootstrap from '../img/bootstrap.png';
 import css from '../img/css.png';
-import flask from '../img/flask-icon.jpg';
+import flask from '../img/flask.png';
 import html from '../img/html.png';
 import javascript from '../img/javascript.webp';
-import jwt from '../img/jwt.webp';
 import python from '../img/python.webp';
 import react from '../img/react.png';
 import sql from '../img/sql.png';
-import angular from '../img/angular.jpg';
-import django from '../img/django.jpg';
+import angular from '../img/angular.png';
+import django from '../img/django.png';
 import fastapi from '../img/fastapi.png';
 import typescript from '../img/typescript.png';
 
-const Technologies = ({ theme }) => {
+const backendTechnologies = [
+  { name: 'Python', img: python },
+  { name: 'Flask', img: flask },
+  { name: 'Django', img: django },
+  { name: 'PostgreSQL', img: sql },
+  { name: 'FastAPI', img: fastapi },
+];
 
+const frontendTechnologies = [
+  { name: 'JavaScript', img: javascript },
+  { name: 'React', img: react },
+  { name: 'Angular', img: angular },
+  { name: 'TypeScript', img: typescript },
+  { name: 'HTML', img: html },
+  { name: 'CSS', img: css },
+];
+
+const Technologies = ({ theme }) => {
   return (
     <div id="technologies" className={`container-fluid technologies-component ${theme === 'dark' ? 'dark-theme' : 'light-theme'}`}>
-      <h2>Technologies</h2>
-      <div className="technologies-wheel">
+      <h2 className='title-tech'>Tecnologías</h2>
+      <div className="technologies-columns">
+        <div className="technologies-section">
+          <h4 className='title-end'>Backend</h4>
+          <div className="technologies-grid">
+            {backendTechnologies.map((tech, index) => (
+              <div key={index} className="technology-item">
+                <img src={tech.img} alt={tech.name} className="technology-img" />
+                <span className="technology-name">{tech.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="technologies-section">
+          <h4 className='title-end'>Frontend</h4>
+          <div className="technologies-grid">
+            {frontendTechnologies.map((tech, index) => (
+              <div key={index} className="technology-item">
+                <img src={tech.img} alt={tech.name} className="technology-img" />
+                <span className="technology-name">{tech.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
